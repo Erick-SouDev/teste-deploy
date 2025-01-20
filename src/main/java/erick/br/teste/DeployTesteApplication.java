19b883ab7c936d4cf4f0468a1868d8e19d19cd55
Erick-SouDev <erick1991java@gmail.com>
2025-01-20 17:34:45 -0300

    teste

---
+++ src/main/java/erick/br/teste/DeployTesteApplication.java
@@ -0,0 +1,13 @@
+package erick.br.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erick/br/teste/DeployTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package erick.br.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
